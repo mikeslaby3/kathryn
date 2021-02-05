d2a59bb1c0a20970d9072bcabf1056569d5c9802
Mike Slaby <mikeslaby3@gmail.com>
2021-02-04 23:27:52 -0800

    Page for getting rejected

---
+++ valentines-day/src/pages/imgood.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Layout from '../containers/Layout';
+
+const ImGood = () => {
+  return (
+    <Layout 
+      headerText=":("
+      button1Text="Jesus don't cry ok, let's hear it"
+      button1Link="/yousure/"
+      button2Text="Take me home, country roads"
+      button2Link="/"
+      >
+    </Layout>
+  )
+}
+
+export default ImGood;
