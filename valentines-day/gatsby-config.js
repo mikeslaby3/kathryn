68dbc18e3d542c27e8fb42b7ee8d96c8dadc4807
Mike Slaby <mikeslaby3@gmail.com>
2021-02-04 23:59:52 -0800

    Fixed Google fonts problem

--- valentines-day/gatsby-config.js
+++ valentines-day/gatsby-config.js
@@ -5,12 +5,13 @@ module.exports = {
   plugins: [
     "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-plugin-google-fonts',
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          'Dancing Script',
-          'Londrina Solid',
-          'Patrick Hand'
+          `Dancing Script`,
+          `Londrina Solid`,
+          `Patrick Hand`,
+          `Parisienne`
         ],
         display: 'swap'
       }
