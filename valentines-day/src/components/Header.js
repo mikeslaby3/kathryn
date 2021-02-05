f9820afdc1728f022e1a9e047f50837bca0ab84a
Mike Slaby <mikeslaby3@gmail.com>
2021-02-05 00:00:48 -0800

    Changed border and font of header

--- valentines-day/src/components/Header.js
+++ valentines-day/src/components/Header.js
@@ -3,14 +3,17 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   margin: 25px auto;
+  padding: 10px;
   text-align: center;
   background-color: red;
+  margin-bottom: 75px;
+  border-radius: 12px;
 `;
 
 const PoemLine = styled.h1`
   font-size: 30px;
-  margin-bottom: 75px;
   text-align: center;
+  font-family: 'Parisienne', cursive;
 `;
 
 const header = props => (
