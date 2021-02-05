3df064d7fbc847a73f9b0ea453257b01480cf631
Mike Slaby <mikeslaby3@gmail.com>
2021-02-04 23:24:18 -0800

    Added more Gatsby plugins

--- valentines-day/gatsby-config.js
+++ valentines-day/gatsby-config.js
@@ -2,5 +2,18 @@ module.exports = {
   siteMetadata: {
     title: "kathryn",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          'Dancing Script',
+          'Londrina Solid',
+          'Patrick Hand'
+        ],
+        display: 'swap'
+      }
+    }
+  ],
 };
