4a77ab18be60d2864de08f9ffe8da91a4a2708fd
Mike Slaby <mikeslaby3@gmail.com>
2021-02-05 00:00:34 -0800

    Made a container component for the buttons

---
+++ valentines-day/src/containers/Buttons.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+
+import Button from '../components/Button';
+
+const ButtonContainer = styled.div`
+    margin: 50px auto;
+    width: 350px;
+    height: 100%;
+    background-color: blue;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+`;
+
+const layout = (props) => (
+    <Fragment>
+        <ButtonContainer>
+            <Button 
+                name={props.b1t}
+                linkedPage={props.b1l}>
+            </Button>
+            <Button 
+                name={props.b2t}
+                linkedPage={props.b2l}>
+            </Button>
+        </ButtonContainer>
+    </Fragment>
+)
+
+export default layout;
