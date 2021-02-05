cb87f772d25345b0381534efd2763e956f463b7c
Mike Slaby <mikeslaby3@gmail.com>
2021-02-05 00:00:01 -0800

    Changed button font

--- valentines-day/src/components/Button.js
+++ valentines-day/src/components/Button.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Link } from "gatsby";
 
 const Button = styled.button`
-  font-size: 20px;
+  font-size: 25px;
   color: red;
   background-color: #DEDEDE;
   display: block;
@@ -12,13 +12,14 @@ const Button = styled.button`
   cursor: pointer;
   border: 3px solid black;
   border-radius: 12px;
+  font-family: 'Londrina Solid', cursive;
 `;
 
 const button = (props) => (
       <Button>
         <Link 
         to={props.linkedPage}
-        style={{ textDecoration: 'none', color: 'red'}}>
+        style={{ textDecoration: 'none', color: 'black'}}>
           {props.name}
         </Link>
       </Button>
